e7e058e5f9e1a82d41e81d57b91f998f4d1e6795
sugarslaurence <sugarslaurence@gmail.com>
2018-03-18 11:34:43 +0000

    18/03/2018

--- Main.py
+++ Main.py
@@ -1,35 +1,16 @@
+from Compiler import *
+from Import import *
 from Optimise import *
-from Files import *
-from pprint import pprint as pp
 
-def graph_hopper_request_builder(vehicles):
+services = read_services()
+vehicles = read_vehicles()
 
-    default_body = default
-    new_vehicle_template = default_body['vehicles'][0]  # dict
-
-
-    vehicles_list = []
-
-    for x in range(0, vehicles):
-
-        vehicle = new_vehicle_template.copy()
-        vehicle['vehicle_id'] = str(x + 1)
-        vehicles_list.append(vehicle)
-
-    default_body["vehicles"] = vehicles_list
-    print(pp(default_body))
-
-    return default_body
-
-graph_hopper_request_builder(5)
-
-body = graph_hopper_request_builder(5)
-id = start_optimiser(body)
+request = graph_hopper_request_builder(vehicles, services)
+id = start_optimiser(request)
+print(id)
 result = get_results(id)
+#kpis = schedule_kpis(result)
+
 print(result)
-
-
-
-
-
+#print(kpis)
 
